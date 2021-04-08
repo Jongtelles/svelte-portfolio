909b8a3b2fc4b2634ac9ac509ad996fcab4b6db2
Jon Telles <jongtelles@gmail.com>
2021-04-08 16:20:34 -0400

    added quote

--- src/lib/_data.ts
+++ src/lib/_data.ts
@@ -1,77 +1,83 @@
 export const words = [
-	{
-		cite: 'Grace Hopper',
-		words: "The most damaging phrase is...it's always been done this way."
-	},
-	{
-		cite: 'Ron Jeffries',
-		words: 'Code never lies; comments sometimes do.'
-	},
-	{
-		cite: 'Louis Srygley',
-		words:
-			'Without requirements or design, programming is the art of adding bugs to an empty text file.'
-	},
-	{
-		cite: 'Bjarne Stroustrup',
-		words:
-			'There are only two kinds of languages: the ones people complain about and the ones nobody uses.'
-	},
-	{
-		cite: 'Nicoll Hunt',
-		words:
-			'The first step of any project is to grossly underestimate its complexity and difficulty.'
-	},
-	{
-		cite: 'Neal Ford',
-		words: 'Developers are drawn to complexity like moths to a flame, often with the same outcome.'
-	},
-	{
-		cite: 'Kent Beck',
-		words: 'Optimism is an occupational hazard of programming; feedback is the treatment.'
-	},
-	{
-		cite: 'John Johnson',
-		words: 'First solve the problem, then write the code.'
-	},
-	{
-		cite: 'Kyle Simpson',
-		words: "There's nothing more permanent than a temporary hack."
-	},
-	{
-		cite: 'Alan J. Perlis',
-		words: 'There are two ways to write error-free programs; only the third one works.'
-	},
-	{
-		cite: 'Jeff Sickel',
-		words: 'Deleted code is debugged code.'
-	},
-	{
-		cite: 'Bram Cohen',
-		words: 'The trick is to fix the problem you have, rather than the problem you want.'
-	},
-	{
-		cite: 'Michio Kaku',
-		words:
-			'You can mass-produce hardware; you cannot mass-produce software; you cannot mass-produce the human mind.'
-	},
-	{
-		cite: 'Doug Linder',
-		words:
-			'A good programmer is someone who always looks both ways before crossing a one-way street.'
-	},
-	{
-		cite: 'Karolina Szczur',
-		words:
-			'Writing software as if we are the only person that ever has to comprehend it is one of the biggest mistakes and false assumptions that can be made.'
-	},
-	{
-		cite: 'J. Chambers',
-		words:
-			'Prolific developers don’t always write a lot of code, instead they solve a lot of problems. The two things are not the same.'
-	},
-	{
-		cite: 'Donald Knuth',
-		words: 'Programs are meant to be read by humans and only incidentally for computers to execute.'
-	}
+  {
+    cite: "Grace Hopper",
+    words: "The most damaging phrase is...it's always been done this way.",
+  },
+  {
+    cite: "Ron Jeffries",
+    words: "Code never lies; comments sometimes do.",
+  },
+  {
+    cite: "Louis Srygley",
+    words:
+      "Without requirements or design, programming is the art of adding bugs to an empty text file.",
+  },
+  {
+    cite: "Bjarne Stroustrup",
+    words:
+      "There are only two kinds of languages: the ones people complain about and the ones nobody uses.",
+  },
+  {
+    cite: "Nicoll Hunt",
+    words:
+      "The first step of any project is to grossly underestimate its complexity and difficulty.",
+  },
+  {
+    cite: "Neal Ford",
+    words: "Developers are drawn to complexity like moths to a flame, often with the same outcome.",
+  },
+  {
+    cite: "Kent Beck",
+    words: "Optimism is an occupational hazard of programming; feedback is the treatment.",
+  },
+  {
+    cite: "John Johnson",
+    words: "First solve the problem, then write the code.",
+  },
+  {
+    cite: "Kyle Simpson",
+    words: "There's nothing more permanent than a temporary hack.",
+  },
+  {
+    cite: "Alan J. Perlis",
+    words: "There are two ways to write error-free programs; only the third one works.",
+  },
+  {
+    cite: "Jeff Sickel",
+    words: "Deleted code is debugged code.",
+  },
+  {
+    cite: "Bram Cohen",
+    words: "The trick is to fix the problem you have, rather than the problem you want.",
+  },
+  {
+    cite: "Michio Kaku",
+    words:
+      "You can mass-produce hardware; you cannot mass-produce software; you cannot mass-produce the human mind.",
+  },
+  {
+    cite: "Doug Linder",
+    words:
+      "A good programmer is someone who always looks both ways before crossing a one-way street.",
+  },
+  {
+    cite: "Karolina Szczur",
+    words:
+      "Writing software as if we are the only person that ever has to comprehend it is one of the biggest mistakes and false assumptions that can be made.",
+  },
+  {
+    cite: "J. Chambers",
+    words:
+      "Prolific developers don’t always write a lot of code, instead they solve a lot of problems. The two things are not the same.",
+  },
+  {
+    cite: "Donald Knuth",
+    words:
+      "Programs are meant to be read by humans and only incidentally for computers to execute.",
+  },
+  {
+    cite: "Martin Fowler",
+    words:
+      "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  },
 ];
